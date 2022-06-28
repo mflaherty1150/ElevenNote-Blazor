17b0149520ff5f53e845bd01c50f8a027f37f7ad
Martin Flaherty <flaherty.martin@gmail.com>
2022-06-28 15:42:51 -0400

    End section EN1.xx

---
+++ ElevenNoteWebApp/Server/Models/CategoryEntity.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNoteWebApp.Server.Models
+{
+    public class CategoryEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+    }
+}
---
+++ ElevenNoteWebApp/Server/Models/NoteEntity.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNoteWebApp.Server.Models
+{
+    public class NoteEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string OwnerId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Content { get; set; }
+        public int CategoryId { get; set; }
+        public virtual CategoryEntity Category { get; set; }
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
